e19956db01efa187c637902903b40adb186cc4d6
Alex Waller <alex@artofalexwaller.com>
2021-04-30 12:53:07 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -2,5 +2,42 @@
 import React from 'react'
 
 class ClickityClick extends React.Component {
+  
+     constructor() {
+         super();
+
+         // Define the initial state
+         this.state = {
+             hasBeenClicked: false
+         };
+     }
+
+     handleClick = () => {
+       // Update our state here...
+       this.setState({
+           hasBeenClicked: true
+       })
+     };
+     
+// this.setState() is a function available to all React 
+// Components that use state, and allows us to let React know that
+// the component state has changed.
+
+    render () {
+      return (
+          <div>
+              <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+              <button onClick={this.handleClick}>Click Me!</button>
+          </div>
+      )
+    }
 
 }
+
+export default ClickityClick
+
+// While a React component can have initial state, the real power
+// is in updating its state - after all, if we didn't need to update
+// the state, the component shouldn't have any state. State is only
+// reserved for data that changes in our component and is visible
+// in the UI.
